db740405e0cafbb40ec0b4dd05ea212df928978d
Sviatoslav <sv_dubov@ukr.net>
2020-02-12 17:29:15 +0200

    Edit BlogTestApplication

--- blogtest/src/main/java/com/dsm/blogtest/BlogTestApplication.java
+++ blogtest/src/main/java/com/dsm/blogtest/BlogTestApplication.java
@@ -2,6 +2,9 @@ package com.dsm.blogtest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @SpringBootApplication
 public class BlogTestApplication {
@@ -9,5 +12,13 @@ public class BlogTestApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BlogTestApplication.class, args);
 	}
+	
+    @Bean
+    public ViewResolver viewResolver() {
+         final InternalResourceViewResolver r = new InternalResourceViewResolver();
+         r.setPrefix("/WEB-INF/jsp/");
+         r.setSuffix(".jsp");
+         return r;
+    }
 
 }
